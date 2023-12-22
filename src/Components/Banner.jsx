f6108bace107b602ef94022cd46b5e3316ad4dc2
Mohammad Ibrahim <ibnabdullah1357@gmail.com>
2023-12-22 19:50:57 +0600

    blog page

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="min-h-screen hero bg-base-200">
             <div className="text-center hero-content">
                 <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Manage Your Tasks Efficiently</h1>
+                    <h1 className="text-4xl font-bold md:text-5xl md:">Manage Your Tasks Efficiently</h1>
                     <p className="py-6">Organize, prioritize, and accomplish tasks effortlessly. Streamline your workflow and boost productivity.</p>
                     <Link to={'/dashboard'}><button className="btn btn-primary">Lets Explore</button></Link>
                 </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,36 @@
-import Banner from "./Banner";
+// Home.js
+
+import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
+    const targetAudience = [
+        { title: 'Developers', description: 'Organize your coding tasks and projects efficiently.' },
+        { title: 'Corporate Professionals', description: 'Stay on top of your work tasks and deadlines.' },
+        { title: 'Students', description: 'Manage your assignments and study schedule effectively.' },
+        { title: 'Entrepreneurs', description: 'Keep track of business tasks and project milestones.' },
+        { title: 'Freelancers', description: 'Coordinate your freelance projects and client deadlines.' },
+        { title: 'Parents', description: 'Organize family tasks and activities for better time management.' },
+    ];
+
     return (
         <div>
-            <Banner></Banner>
+            <Banner />
+
+            <section className="px-8 py-12 bg-gray-100">
+                <div className="container mx-auto">
+                    <h2 className="mb-8 text-3xl font-bold">Who Can Benefit from Our Task Management App?</h2>
+
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                        {targetAudience.map((audience) => (
+                            <div key={audience.title} className="p-6 bg-white rounded-md shadow-md">
+                                <h3 className="mb-4 text-xl font-semibold">{audience.title}</h3>
+                                <p>{audience.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
